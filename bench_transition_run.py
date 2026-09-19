ee51bd4fb756ff6d02b79cf58d456d154547b77d
Jan-Otto Kröpke <mail@jkroepke.de>
2026-09-19 16:22:13 +0200

    bench: print transition-v1 header
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- bench_transition_run.py
+++ bench_transition_run.py
@@ -1,6 +1,8 @@
 import main
 
 
+BENCH_VERSION = "transition-v1"
+
 BENCH_WORLD_SIZE = 32
 BENCH_SPEEDUP = 64
 
@@ -158,6 +160,11 @@ def benchmark_profile(profile):
 
 
 def run_benchmarks():
+    quick_print(
+        "BENCHMARK VERSION",
+        BENCH_VERSION
+    )
+
     quick_print(
         "TRANSITION BENCH SUITE START"
     )
